6c2121ad407b34f140d71e051cffa30260ac44ea
EnricoMagnago <en.magnago@gmail.com>
2015-12-15 08:52:18 +0100

    first implementation

--- js/app.js
+++ js/app.js
@@ -10,17 +10,39 @@ var MailModel = {
    init : function(){
      this.rules = rules;
      this.messages = msgs;
-   }, 
-  
+   },
+	getSize : function(){
+		return this.messages.length;
+	},
+	getMessages : function(){
+		return this.messages;
+	},
+	getMessage : function(i){
+		return this.messages[i];
+	},
+	isSpam : function(message){
+		var res = false;
+		
+		for(var i=0; i < this.rules.length && !res ; i++){
+			
+			if(message.contains(this.rules[i]))
+			  res = true;
+		}
+		return res;
+	},
    /**
     * Filters out messages in the "database" that match the spam rules.
     * @return an array of messages, excluding those that match the filter rules.
     */
     filter : function(){
-      return [];
-    }
-
-  
+		var res = [];
+		for(var i=0; i<MailModel.getSize(); i++){
+			var message = MailModel.getMessage(i);
+			if(!this.isSpam(message))
+				res.push(message);
+		}
+		return res;
+    }  
 };
 
 // Example of usage:
@@ -33,7 +55,38 @@ var MailModel = {
 // you can add here your views and controllers if you decide to do so.
 
 
+var MailView = {
+	init : function(){
+		this.list = $(".result");
+		this.template = "<li>?message?</li>";
+		this.clear();
+	},
+	clear : function(){
+		this.list.empty();
+	},
+	show : function(messages){
+		for(var i=0; i < messages.length; i++){
+			var html = this.template.replace("?message?", messages[i]);
+			this.list.append(html);
+		}
+	}
+};
+
+var MailController = {
+	init : function(){
+		MailModel.init();
+		MailView.init();
+		MailView.show(MailModel.getMessages()); //show all messages
+		$(".btn-filter").on("click", function(){ //filter messages
+			var messages = MailModel.filter();
+			MailView.clear();
+			MailView.show(messages);
+		});
+	}
+};
+
+
 
 $(document).ready(function(){
-
+	MailController.init();
 });
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,5 +1,12 @@
 describe("MailModel", function() {
-  it("should..", function(){
-  });
- 
+	it("should store all messages", function(){
+		MailModel.init();
+		var allMessages = MailModel.getMessages();
+		expect(allMessages).toEqual(["news@spam.com", "carlo@gmail.com", "jessy@compratutto.it", "trentose2@googlegroups.com"]);
+	});
+	it("should remove spam messages", function(){
+		MailModel.init();
+		var messages = MailModel.filter();
+		expect(messages).toEqual(["carlo@gmail.com", "trentose2@googlegroups.com"]);
+	});
 });
